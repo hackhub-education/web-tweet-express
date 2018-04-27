31df960bc2bb18925c4e399f74936ea000ec1780
Michael Chen <cyjdyj@gmail.com>
2018-04-27 11:11:50 -0700

    add middleware

--- app.js
+++ app.js
@@ -1,6 +1,42 @@
 const express = require('express');
 const app = express();
+const path = require('path');
+const logger = require('morgan');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+
+/**
+ * middleware
+ * note: middleware is running in sequence, from top to bottom
+ */
+app.use(logger('dev')); // log requests in server console
+app.use(bodyParser.json()); // parse client request data to json format
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => res.send('Hello World!'));
 
-app.listen(3000, () => console.log('Example app listening on port 3000!'));
+// catch 404 and forward to error handler
+app.use((req, res, next) => {
+  const err =  new Error('Page Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handler
+app.use((err, req, res, next)=> {
+  res.send(err.message);
+});
+
+/**
+ * Get port from environment and store in Express.
+ */
+const port = process.env.PORT || 3000;
+
+/**
+ * Listen on provided port, on all network interfaces.
+ */
+app.listen(port, ()=>{
+  console.log(`Server running on http://localhost:${port}`)
+});
