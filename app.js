9d8888abd301b6086f46ca6b4757acd382c93613
Michael Chen <cyjdyj@gmail.com>
2018-04-27 11:46:16 -0700

    add pug

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/', (req, res) => {
+app.get('/ab+cd', (req, res) => {
   res.render('index', { title: 'Welcome to WebDxD !'})
 });
 
