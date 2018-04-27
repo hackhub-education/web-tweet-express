3bcc36e23462e7caa36a90905399287bae06e349
Michael Chen <cyjdyj@gmail.com>
2018-04-27 11:34:18 -0700

    add html file

--- app.js
+++ app.js
@@ -15,7 +15,9 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/', (req, res) => res.send('Hello World!'));
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'))
+});
 
 // catch 404 and forward to error handler
 app.use((req, res, next) => {
