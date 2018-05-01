12c9f104369ecf92c7a6b305fd064aa55bd1c68b
Michael Chen <cyjdyj@gmail.com>
2018-05-01 11:47:12 -0700

    render view with data

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const path = require('path');
 const logger = require('morgan');
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
+const tweets = require('./tweets');
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -19,8 +20,10 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(logger('dev')); // log requests in server console
 
+app.locals.moment = require('moment');
+
 app.get('/', (req, res) => {
-  res.render('index', { title: 'Welcome to WebDxD !'})
+  res.render('index', { tweets })
 });
 
 app.get('/login', (req, res) => {
