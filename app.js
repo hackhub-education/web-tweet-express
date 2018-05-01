0549c7c3344fbf225c269c9a9e2af36d2ee91807
Michael Chen <cyjdyj@gmail.com>
2018-05-01 10:26:46 -0700

    add all pug file

--- app.js
+++ app.js
@@ -23,6 +23,22 @@ app.get('/', (req, res) => {
   res.render('index', { title: 'Welcome to WebDxD !'})
 });
 
+app.get('/login', (req, res) => {
+  res.render('login');
+});
+
+app.get('/signup', (req, res) => {
+  res.render('signup');
+});
+
+app.get('/profile', (req, res) => {
+  res.render('profile');
+});
+
+app.get('/profile/edit', (req, res) => {
+  res.render('editProfile');
+});
+
 // catch 404 and forward to error handler
 app.use((req, res, next) => {
   const err =  new Error('Page Not Found');
