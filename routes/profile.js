15054a0bfc3d592145c4003a6df9e0d73bb1fe1d
Michael Chen <cyjdyj@gmail.com>
2018-05-02 13:53:29 -0700

    update and render user profile

--- routes/profile.js
+++ routes/profile.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
-const tweets = require('../tweets');
+const Users = require('../models/users');
 const utils = require('../utils');
 
 router.get('/', utils.requireLogin, (req, res) => {
@@ -10,6 +10,17 @@ router.get('/', utils.requireLogin, (req, res) => {
 
 router.get('/edit', utils.requireLogin, (req, res) => {
   res.render('editProfile');
+});
+
+// update user profile
+router.post('/:id', utils.requireLogin, (req, res) => {
+  Users.update({ _id: req.params.id }, req.body, (err) => {
+    if(err) {
+      return next(err);
+    } else {
+      return res.redirect('/profile')
+    }
+  });
 });
 
 module.exports = router;
