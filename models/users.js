912a36a004cf44940c9b13755f91e91d94a04615
Yan Hong <onlyhongyan@gmail.com>
2018-05-13 04:53:45 -0700

    udpate sample avatar

--- models/users.js
+++ models/users.js
@@ -8,7 +8,7 @@ const UsersSchema = new Schema({
     username: { type: String, required: true },
     location: String,
     bio: String,
-    avatarUrl: { type: String, default: '/img/webdxd.png' },
+    avatarUrl: { type: String, default: '/img/sample-avatar.png' },
     tweets: [{ type: Schema.Types.ObjectId, ref: 'Tweets' }]
 });
 
--- routes/tweet.js
+++ routes/tweet.js
@@ -7,7 +7,7 @@ const router = express.Router();
 // get all tweets
 router.get('/', async (req, res) => {
   try {
-    const tweets = await Tweets.find({}).sort('-createdAt').populate('author');
+    const tweets = await Tweets.find({}).populate('author');
     return res.json({ tweets, error: null, success: true })
   } catch (err) {
     return res.json({ error: err, success: false })
@@ -22,6 +22,8 @@ router.post('/', passport.authenticate('jwt', {session: false}), async (req, res
     // save the tweet id to user profile
     req.user.tweets.push(doc._id);
     await req.user.save();
+
+    doc.author = req.user
 
     return res.json({ tweet: doc, error: null, success: true });
   } catch (err) {
